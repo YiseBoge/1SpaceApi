7d3ae749cd3bc5d5f33bebfb2c78c983b7ee5044
yiseboge <Ethiopia1!>
2019-10-12 02:55:44 +0300

    make basic controller methods for Notice related models

--- app/Http/Controllers/Notices/NoticeController.php
+++ app/Http/Controllers/Notices/NoticeController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Notices;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Notices\NoticeResource;
+use App\Models\Notices\Notice;
+use App\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 
 class NoticeController extends Controller
@@ -11,11 +16,12 @@ class NoticeController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        $data = Notice::all();
+        return NoticeResource::collection($data);
     }
 
     /**
@@ -32,22 +38,35 @@ class NoticeController extends Controller
      * Store a newly created resource in storage.
      *
      * @param Request $request
-     * @return Response
+     * @return NoticeResource
      */
     public function store(Request $request)
     {
-        //
+        $user = User::findOrFail($request->input('poster_id'));
+
+        $data = Notice::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        $data->target_date = $request->input('target_date');
+        $data->remind_before = $request->input('remind_before');
+
+        if ($user->postedNotices()->save($data)) {
+            return new NoticeResource($data);
+        }
     }
 
     /**
      * Display the specified resource.
      *
      * @param int $id
-     * @return Response
+     * @return NoticeResource
      */
     public function show($id)
     {
-        //
+        $data = Notice::findOrFail($id);
+        return new NoticeResource($data);
     }
 
     /**
@@ -66,21 +85,34 @@ class NoticeController extends Controller
      *
      * @param Request $request
      * @param int $id
-     * @return Response
+     * @return NoticeResource
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = Notice::findOrFail($id);
+
+        $data->title = $request->input('title');
+        $data->description = $request->input('description');
+        $data->target_date = $request->input('target_date');
+        $data->remind_before = $request->input('remind_before');
+
+        if ($data->save()) {
+            return new NoticeResource($data);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param int $id
-     * @return Response
+     * @return NoticeResource
+     * @throws Exception
      */
     public function destroy($id)
     {
-        //
+        $data = Notice::findOrFail($id);
+        if ($data->delete()) {
+            return new NoticeResource($data);
+        }
     }
 }
