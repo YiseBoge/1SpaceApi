d6e05352ba58f72910de22499110bb50da49f518
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-10-25 10:30:29 +0300

    defined initial relationships

--- app/Models/Project/ProjectFileSubCategory.php
+++ app/Models/Project/ProjectFileSubCategory.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectFileSubCategory extends Model
 {
-    //
+    /**
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Project\ProjectCategory');
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function files()
+    {
+        return $this->morphMany('App\Models\Generics\File', 'fileable');
+    }
 }
