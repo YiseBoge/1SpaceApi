4095c99019f429808b937cc2a1a13ef382c58b6f
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-10-25 10:31:30 +0300

    defined relationships for this model

--- app/Models/Project/ProjectManagementOrganization.php
+++ app/Models/Project/ProjectManagementOrganization.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectManagementOrganization extends Model
 {
-    //
+    /**
+     * @return BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo('App\Models\Project\Project');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function teamMember()
+    {
+        return $this->hasMany('App\Models\Accounts\ProjectTeamMember');
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function files()
+    {
+        return $this->morphMany('App\Models\Generics\File', 'fileable');
+    }
 }
