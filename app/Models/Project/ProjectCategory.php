2753b0b4733b89d928bb0a3ae943407c852b9618
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-10-25 10:21:38 +0300

    added basic relationships for the projectCategory class

--- app/Models/Project/ProjectCategory.php
+++ app/Models/Project/ProjectCategory.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectCategory extends Model
 {
-    //
+    /**
+     * @return HasMany
+     */
+    public function project()
+    {
+        return $this->hasMany('App\Models\Project\Project');
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function files()
+    {
+        return $this->morphMany('App\Models\Generics\File', 'fileable');
+    }
 }
