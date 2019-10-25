fee300c08afc3e0c82d8f4b860506514249ebbb2
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-10-25 10:22:30 +0300

    basic relatioships defined for the project file category model

--- app/Models/Project/ProjectFileCategory.php
+++ app/Models/Project/ProjectFileCategory.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectFileCategory extends Model
 {
-    //
+    /**
+     * @return BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo('App\Models\Project\Project');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function subCategory()
+    {
+        return $this->hasMany('App\Models\Project\ProjectFileSubCategory');
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function files()
+    {
+        return $this->morphMany('App\Models\Generics\File', 'fileable');
+    }
+
 }
