052942f07386e2a07e79179caaace3f4ec82a5a3
MULUALEM-TEKLE <mulualemtekle261@gmail.com>
2019-10-25 10:31:13 +0300

    relationships defiend

--- app/Models/Project/ProjectLocation.php
+++ app/Models/Project/ProjectLocation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectLocation extends Model
 {
-    //
+    /**
+     * @return BelongsTo
+     */
+    public function project()
+    {
+        return $this->belongsTo('App\Models\Project\Project');
+    }
 }
